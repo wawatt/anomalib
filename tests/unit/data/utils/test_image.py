84d4eb147cda5e33090397d63f22022a03f0415a
MFr <49654038+AveraGE0@users.noreply.github.com>
2025-07-03 12:03:13 +0200

    🐞fix(data): Fixed bug where MVTecLOCO GT Masks were not loaded correctly (#2805)
    * Fixed bug where MVTecLOCO GT Masks were not loaded
    
    - Adapted readm_mask function to recognize pixels with values greater 0 as anomalous
    - The MVTecLOCO dataset contains anomalous pixel values smaller than 255. Before, this would lead to GT pixels not being loaded
    
    Signed-off-by: Mika Rosin <m.f.rosin@student.vu.nl>
    
    * Added tests for the read_mask function
    
    Signed-off-by: Mika Rosin <m.f.rosin@student.vu.nl>
    
    ---------
    
    Signed-off-by: Mika Rosin <m.f.rosin@student.vu.nl>

--- tests/unit/data/utils/test_image.py
+++ tests/unit/data/utils/test_image.py
@@ -6,8 +6,9 @@
 from pathlib import Path
 
 import pytest
+from pytest_mock import MockerFixture
 
-from anomalib.data.utils.image import get_image_filenames
+from anomalib.data.utils.image import Image, get_image_filenames, np, read_mask, torch
 
 
 class TestGetImageFilenames:
@@ -46,3 +47,41 @@ class TestGetImageFilenames:
         filename = dataset_path / "avenue/ground_truth_demo/testing_label_mask/1_label.mat"
         with pytest.raises(ValueError, match=r"``filename`` is not an image file*"):
             get_image_filenames(filename)
+
+
+class TestReadMask:
+    """Tests for ``read_mask`` function."""
+
+    @staticmethod
+    def test_mask_conversion_as_numpy(mocker: MockerFixture) -> None:
+        """Test that the function correctly converts the image to a numpy array."""
+        # Sample image: [[0, 236], [255, 0]]
+        mocker.patch(
+            "anomalib.data.utils.image.Image.open",
+            return_value=Image.fromarray(np.array([[0, 236], [255, 0]], dtype=np.uint8)),
+        )
+
+        # Mocks are setup, call function to test!
+        result = read_mask("dummy_path.png", as_tensor=False)
+
+        assert isinstance(result, np.ndarray)
+        np.testing.assert_array_equal(result, np.array([[0, 236], [255, 0]]))
+
+    @staticmethod
+    def test_mask_conversion_as_tensor(mocker: MockerFixture) -> None:
+        """Test that the function correctly converts the image to a tensor (normalized to 0, 1).
+
+        Test cases:
+        - Value 0 remains non-anomalous (0)
+        - Value 236 (>0) becomes anomalous (1)
+        - Value 255 becomes anomalous (1)
+        """
+        mocker.patch(
+            "anomalib.data.utils.image.Image.open",
+            return_value=Image.fromarray(np.array([[0, 236], [255, 0]], dtype=np.uint8)),
+        )
+        result = read_mask("dummy_path.png", as_tensor=True)
+
+        assert isinstance(result, torch.Tensor)
+        assert result.dtype == torch.uint8
+        assert torch.equal(result, torch.tensor([[0, 1], [1, 0]], dtype=torch.uint8))
